cf7f664554da3db57eaa520a08145ed20bc50c0a
stevengogogo <stevengogogo@users.noreply.github.com>
2021-04-09 12:04:34 +0000

    deploy: 209f7afc76c6ab9136531d7cdf63b5ee6c6d29ec

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['find_5fith_16',['find_ith',['../linkedarray_8h.html#a494820db8b541be04d62af798381a183',1,'linkedarray.c']]]
+  ['find_5flnkarr_5fith_16',['find_LnkArr_ith',['../linkedarray_8h.html#a0b1aaa8815d1669a626d45981c948756',1,'linkedarray.c']]]
 ];

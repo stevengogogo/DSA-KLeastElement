d488250733c072424c27c8c5ac3b6bef48cbd218
stevengogogo <stevengogogo4321@gmail.com>
2021-04-09 15:37:01 +0800

    implement quick sort

--- src/utils.h
+++ src/utils.h
@@ -3,7 +3,7 @@
  * @author Shao-Ting Chiu (r07945001@ntu.edu.tw)
  * @brief General Utilities for C language
  * @version 0.1
- * @date 2021-03-08
+ * @date 2021-04-10
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -11,26 +11,15 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-/**
- * @brief Store extreme value
- * @param extreme extreme value
- * @param arg argument
- */
-typedef struct {
-    int extreme;
-    int arg;
-} extm;
 
-/**
- * @brief 
- * 
- * @param a 
- * @param b 
- * @return extm 
- */
-extm max(int* a,int* b);
-extm min(int*,int*);
+/** * QuickSort. Reference: This function is modified from https://openhome.cc/Gossip/AlgorithmGossip/QuickSort3.htm#C */
+void quicksort(int arr[], int left, int right);
 
+/** * Combine fragments. This function is modifed from: https://openhome.cc/Gossip/AlgorithmGossip/QuickSort3.htm#C */
+int partition(int number[], int left, int right);
+
+/** * Switch the value store in `x` and `y`. */
+void swap(int* x, int* y);
 
 
 #endif

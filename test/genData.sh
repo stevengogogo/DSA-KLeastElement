e218e096d4324549183bc673f1158d6fbe96b6ee
stevengogogo <stevengogogo4321@gmail.com>
2021-04-12 22:22:43 +0800

    Bug: query error

--- test/genData.sh
+++ test/genData.sh
@@ -3,7 +3,7 @@
 echo "generate data"
 for i in {11..13};
 do
-    /home/ubuntu/.local/bin/julia test/dataGen.jl 50000 50 test/data/$i.in test/data/$i.out 
+    /home/ubuntu/.local/bin/julia test/dataGen.jl 200 500 test/data/$i.in test/data/$i.out 
 
     echo "Create: test/data/$i.in test/data/$i.out"
 

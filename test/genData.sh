3494ae6fe6a3cfc7ac9336279a1bc074b45c2eed
stevengogogo <stevengogogo4321@gmail.com>
2021-04-12 22:50:03 +0800

    Bug: Wrong answer

--- test/genData.sh
+++ test/genData.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "generate data"
-for i in {14..15};
+for i in {16..18};
 do
-    /home/ubuntu/.local/bin/julia test/dataGen.jl 200 5000 test/data/$i.in test/data/$i.out 
+    /home/ubuntu/.local/bin/julia test/dataGen.jl 5000 2000 test/data/$i.in test/data/$i.out 
 
     echo "Create: test/data/$i.in test/data/$i.out"
 

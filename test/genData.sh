bdd770064bb9cf4e103e8973d894d97cccdd2c0b
stevengogogo <stevengogogo4321@gmail.com>
2021-04-12 22:36:22 +0800

    Pass: more cmds

--- test/genData.sh
+++ test/genData.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "generate data"
-for i in {11..13};
+for i in {14..15};
 do
-    /home/ubuntu/.local/bin/julia test/dataGen.jl 200 500 test/data/$i.in test/data/$i.out 
+    /home/ubuntu/.local/bin/julia test/dataGen.jl 200 5000 test/data/$i.in test/data/$i.out 
 
     echo "Create: test/data/$i.in test/data/$i.out"
 

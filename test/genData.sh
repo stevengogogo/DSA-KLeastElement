946481e74e852ca661702f320629a0775bbbf168
stevengogogo <stevengogogo4321@gmail.com>
2021-04-12 15:16:43 +0800

    make more data: extend the init-range and cmds

--- test/genData.sh
+++ test/genData.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "generate data"
-for i in {3..4};
+for i in {5..7};
 do
-    /home/ubuntu/.local/bin/julia test/dataGen.jl 150 5 test/data/$i.in test/data/$i.out
+    /home/ubuntu/.local/bin/julia test/dataGen.jl 6000 50 test/data/$i.in test/data/$i.out
 
     echo "Create: test/data/$i.in test/data/$i.out"
 

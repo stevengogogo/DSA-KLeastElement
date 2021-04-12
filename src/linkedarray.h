e6262177ad429dca09eae84a562e29e5c99f6175
stevengogogo <stevengogogo4321@gmail.com>
2021-04-12 15:19:19 +0800

    Bug: Query ValueError

--- src/linkedarray.h
+++ src/linkedarray.h
@@ -18,7 +18,7 @@
 #include <string.h>
 #include "utils.h"
 
-#define subN 2000
+#define subN 100
 
 /** * Node with arrays.
  * @param len length of stored data
--- test/genData.sh
+++ test/genData.sh
@@ -3,7 +3,7 @@
 echo "generate data"
 for i in {5..7};
 do
-    /home/ubuntu/.local/bin/julia test/dataGen.jl 6000 50 test/data/$i.in test/data/$i.out
+    /home/ubuntu/.local/bin/julia test/dataGen.jl 340 6 test/data/$i.in test/data/$i.out
 
     echo "Create: test/data/$i.in test/data/$i.out"
 

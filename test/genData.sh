25cd960e008af680303ad2529f43d53ebc59c8df
stevengogogo <stevengogogo4321@gmail.com>
2021-04-13 17:13:48 +0800

    upload data

--- test/genData.sh
+++ test/genData.sh
@@ -3,7 +3,7 @@
 echo "generate data"
 for i in {22..100};
 do
-    /home/ubuntu/.local/bin/julia test/dataGen.jl 50000 50000 test/data/$i.in test/data/$i.out 
+    /home/ubuntu/.local/bin/julia test/dataGen.jl 5 50000 test/data/$i.in test/data/$i.out 
 
     echo "Create: test/data/$i.in test/data/$i.out"
 
